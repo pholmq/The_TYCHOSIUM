8ca935019dac54d65b7784be52deff6a1d947c4d
pholmq <pholmq@gmail.com>
2022-08-19 14:12:59 +0200

    removed normal specular map from Earth

--- src/Earth.jsx
+++ src/Earth.jsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
 import { Sphere, useTexture } from "@react-three/drei";
 import PlanetCamera from "./PlanetCamera";
+import { DoubleSide } from "three";
 
 // import PlanetCamera from "./PlanetCamera";
 
@@ -14,24 +15,24 @@ export function Earth(props) {
     earthRef.current.rotation.y -= 0.0005;
     cloudsRef.current.rotation.y -= 0.0004;
   });
-  const [cloudsMap, colorMap, normalMap, specularMap] = useTexture([
-    // map: "/world_topo_bathy_200407_3x5400x2700.jpg"
+  const [cloudsMap, colorMap] = useTexture([
     "/textures/8k_earth_clouds.jpg",
-    "/textures/8k_earth_daymap.jpg",
-    "/textures/8k_earth_normal_map.jpg",
-    "/textures/8k_earth_specular_map.jpg"
+    "/textures/8k_earth_daymap.jpg"
+    // "/textures/8k_earth_normal_map.jpg",
+    // "/textures/8k_earth_specular_map.jpg"
   ]);
 
   return (
     <>
       <mesh ref={earthRef} scale="1">
         <sphereGeometry args={[props.size, 128, 128]} />
-        <meshPhongMaterial specularMap={specularMap} />
+        {/* <meshPhongMaterial specularMap={specularMap} /> */}
         <meshStandardMaterial
           map={colorMap}
-          normalMap={normalMap}
+          // normalMap={normalMap}
           metalness={0.4}
           roughness={0.7}
+          // side={DoubleSide}
         />
         <PlanetCamera />
       </mesh>
@@ -40,10 +41,10 @@ export function Earth(props) {
         <sphereGeometry args={[props.size + 0.03, 64, 64]} />
         <meshPhongMaterial
           map={cloudsMap}
-          opacity={0.3}
+          opacity={0.2}
           depthWrite={true}
           transparent={true}
-          // side={THREE.DoubleSide}
+          // side={DoubleSide}
         />
       </mesh>
 
