ad4ef8e7c43a49270da810920936575fa060f8fd
pholmq <pholmq@gmail.com>
2022-08-20 12:44:40 +0200

    Temp RA Dec labels on planets

--- src/Planet.jsx
+++ src/Planet.jsx
@@ -36,7 +36,7 @@ export function Planet(props) {
         {hovered && (
           <Html position={[-10, 0, 0]}>
             <div className="planetLabel">
-              {props.name} <br /> RA: XXhXXmXXs <br /> Dec: +XX°XX'XX"
+              {props.name} <br /> RA:&nbsp;XXhXXmXXs Dec:&nbsp;+XX°XX'XX"
             </div>
           </Html>
         )}
